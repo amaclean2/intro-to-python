463fad7ecd85c6678be0bcce0bc8f3468e1acacd
Andrew Maclean <andrew.n.maclean@gmail.com>
2020-04-11 14:21:43 -0700

    descriptions

--- lessonTwo.py
+++ lessonTwo.py
@@ -1,5 +1,12 @@
+# There is a math operator, mod (%) that gives you the remainder of a division
+# operation.
+# Ex: 
+# 3 % 2 = 1
+# 5 % 3 = 2
+
 # This exercise will be to write a program that prints all the even
 # numbers from 0 to 10
+# Use the mod operator and if-else statements to determine if a number is even or odd
 
 i = 0
 while i <= 10 :
