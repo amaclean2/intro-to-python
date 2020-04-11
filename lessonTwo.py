9144365e006033ffa3c9ef75aaaea826e25f2fec
Andrew Maclean <andrew.n.maclean@gmail.com>
2020-04-11 14:37:44 -0700

    modular arithmatic

--- lessonTwo.py
+++ lessonTwo.py
@@ -4,6 +4,14 @@
 # 3 % 2 = 1
 # 5 % 3 = 2
 
+# We can use this to identify if a number is even or odd by checking if a number % 2 is 1 or 0
+
+# if a % 2 == 1 :
+#   a is odd
+# if a % 2 == 0 :
+#   a is even
+
+
 # This exercise will be to write a program that prints all the even
 # numbers from 0 to 10
 # Use the mod operator and if-else statements to determine if a number is even or odd
