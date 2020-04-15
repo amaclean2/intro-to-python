35bb463b2bb5e26db3b479d4e58c37f3ed45a06e
Andrew MacLean <andrewmaclean@DreamBook.local>
2020-04-14 18:14:33 -0700

    changed array to list

--- lessonFive.py
+++ lessonFive.py
@@ -1,21 +1,23 @@
-myArray = []
+myList = []
 
-# Complete a function that adds elements to the array, 'myArray'
-# only if the length of the array is less than 5.
-def addToArray(element) :
+# Complete a function that adds elements to the list, 'myList'
+# only if the length of the list is less than 5.
+def addToList(element) :
     # your code here
 
 
-# Complete a function that removes the last element from the array
-# 'myArray', and returns it unless there is one left, then leave it.
-def removeFromArray() :
+# Complete a function that removes the last element from the list
+# 'myList', and returns it unless there is one left, then leave it.
+def removeFromList() :
     # your code here
+
 
 
 # Complete a function that returns the index of 'element'.
-# If the array isn't that long then return -1.
+# If the list doesn't contain the element then return -1.
 def getIndex(element) :
     # your code here
+
 
 
 # Complete a function that calls getElement() and prints the value
@@ -25,44 +27,44 @@ def printIndex(element) :
 
 
 # Complete a function that removes the element 'element' from the
-# array and returns it. If the element doesn't exist, then return -1.
+# list and returns it. If the element doesn't exist, then return -1.
 def removeElement(element) :
     # your code here
 
 
 #-----#
 
-addToArray(5)
+addToList(5)
 print("added 5: ", myArray)
 
-addToArray(12)
+addToList(12)
 print("added 12: ", myArray)
 
-removeFromArray()
+removeFromList()
 print("popped: ", myArray)
 
-removeFromArray()
+removeFromList()
 print("popped: ", myArray)
 
-addToArray(12)
+addToList(12)
 print("added 12: ", myArray)
 
-addToArray(13)
+addToList(13)
 print("added 13: ", myArray)
 
-addToArray(6)
+addToList(6)
 print("added 6: ", myArray)
 
-addToArray(2)
+addToList(2)
 print("added 2: ", myArray)
 
-addToArray(7)
+addToList(7)
 print("added 7: ", myArray)
 
-removeFromArray()
+removeFromList()
 print("popped: ", myArray)
 
-removeFromArray()
+removeFromList()
 print("popped: ", myArray)
 
 print("13: ")
